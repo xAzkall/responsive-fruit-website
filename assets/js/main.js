9374417437d7d26f96814bff9f2c5752be04a82a
bulanowski <kontakt@bulanowski.pl>
2023-10-26 21:41:12 +0200

    version-1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,45 @@
 /*=============== SHOW MENU ===============*/
+const menu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    menu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  menu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => {
+  n.addEventListener("click", linkAction);
+});
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the shadow-header class
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
