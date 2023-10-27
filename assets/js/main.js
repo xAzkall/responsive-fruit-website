41a451d1a53af0bc3c06640147d8ebf31aecc0f1
bulanowski <kontakt@bulanowski.pl>
2023-10-27 21:45:07 +0200

    version-1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,9 +37,83 @@ const shadowHeader = () => {
 window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is heigher then 350 viewport hight, add the show-scroll class to the tag withe the scroll-up id
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        `.nav__menu a[href*=${sectionId}]`
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button"),
+  darkTheme = "dark-theme",
+  iconTheme = "ri-sun-line";
 
+// Previously selectet topic (if user selected)
+let selectedTheme = localStorage.getItem("selected-theme"),
+  selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light",
+  getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-theme";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // We validate if the user previosly chose a topic
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    darkTheme
+  );
+}
+
+// Active / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(".home__data, .join__container, .footer");
+sr.reveal(".home__img", { origin: "bottom" });
+sr.reveal(".enjoy__card, .popular__card", { interval: 10 });
+sr.reveal(".about__data", { origin: "right" });
+sr.reveal(".about__img", { origin: "left" });
